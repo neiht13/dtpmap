3652f0d6a0fae13c9d3548edecca9b781fb58bf5
EvElfie <40723286+neiht13@users.noreply.github.com>
2023-10-03 11:02:19 +0700

    Update get-all.ts

--- src/pages/api/list-location/get-all.ts
+++ src/pages/api/list-location/get-all.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             })
             res.status(200).json({result})
         } catch (err) {
-            res.status(500).json({error: 'failed to load data'})
+            res.status(500).json({error: 'failed to load data', result: []})
         }
     }
     else {
